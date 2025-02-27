18e4be3ac0a5b773039ba3b98869a8d193302aa7
Thristan14 <thristanmenle@gmail.com>
2025-02-26 19:56:20 -0600

    Sexto

--- src/torreshanoi/TorresHanoi.java
+++ src/torreshanoi/TorresHanoi.java
@@ -114,5 +114,7 @@ public class TorresHanoi extends JPanel {
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        
+        //programación
     }
 }
